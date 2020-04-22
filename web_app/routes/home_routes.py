49924709ca5aa50f5eea6e0c25e0beb16543f458
jpeone <jesse.gene.peone@gmail.com>
2020-04-21 20:14:08 -0700

    setup the home_routes and the book_routes

---
+++ web_app/routes/home_routes.py
@@ -0,0 +1,12 @@
+from flask import Blueprint
+
+home_routes = Blueprint('home_routes', __name__)
+
+@home_routes.route('/')
+def hello_world():
+    print('made it to the main page')
+    return 'Hello World'
+
+@home_routes.route('/about')
+def about():
+    return('About me')
