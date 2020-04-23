da94074e35f2d2c3807a61a1cdc67a39c85c5439
jpeone <jesse.gene.peone@gmail.com>
2020-04-22 20:25:42 -0700

    Set up the DB with flask

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # ds-unit-3-sprint-3-twitoff
+
+## Setup
+TODO: virtual env set up
+
+DB setup:
+```sh
+export FLASK_APP=web_app
+flask db init
+flask db migrate
+flask db upgrade
+```
+
+## Usage
+```sh
+export FLASK_APP=web_app
+flask run
+```
--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,10 +1,22 @@
 from flask import Flask
+import os
 
+from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
 
+#couldn't get this to work with absolute or os generated file path
+DATABASE_URI = "sqlite:///development.db"
+
 def create_app():
     app = Flask(__name__)
+
+    print(DATABASE_URI)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
     return app
