e64d449e3dad2960d0a0e51a364ff34796130ec5
jpeone <jesse.gene.peone@gmail.com>
2020-04-24 15:38:45 -0700

    basic embeddings implemented

--- web_app/routes/twitter_routes.py
+++ web_app/routes/twitter_routes.py
@@ -1,6 +1,7 @@
 # web_app/routes/twitter_routes.py
 from web_app.models import db, User, Tweet, parse_records
 from web_app.services.twitter_service import api as twitter_api
+from web_app.services.basilica_service import conn as basilica_connection
 
 from flask import Blueprint, jsonify#, request, render_template, flash, redirect
 
@@ -34,10 +35,12 @@ def fetch_user_data(screen_name):
         db_tweet = Tweet.query.get(status.id) or Tweet(id=status.id)
         db_tweet.user_id = status.author.id # or db_user.id
         db_tweet.full_text = status.full_text
-        #embedding = basilica_client.embed_sentence(status.full_text, model="twitter") # todo: prefer to make a single request to basilica with all the tweet texts, instead of a request per tweet
+
+        #fetching corresponding embeddings.
+        embedding = basilica_connection.embed_sentence(status.full_text, model="twitter") # todo: prefer to make a single request to basilica with all the tweet texts, instead of a request per tweet
         #embedding = embeddings[counter]
-        #print(len(embedding))
-        #db_tweet.embedding = embedding
+        print(len(embedding))
+        db_tweet.embedding = embedding
         db.session.add(db_tweet)
         #counter+=1
         
