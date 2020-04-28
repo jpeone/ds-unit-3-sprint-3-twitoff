41f16c9dd26c505b9fe69d2d6b66d73f21d0fa3e
jpeone <jesse.gene.peone@gmail.com>
2020-04-27 19:22:18 -0700

    admin routes, and reset db

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -5,6 +5,7 @@ from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
 from web_app.routes.twitter_routes import twitter_routes
+from web_app.routes.admin_routes import admin_routes
 
 #couldn't get this to work with absolute or os generated file path
 DATABASE_URI = "sqlite:///development.db"
@@ -25,6 +26,7 @@ def create_app():
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
     app.register_blueprint(twitter_routes)
+    app.register_blueprint(admin_routes)
     return app
 
 if __name__ == "__main__":
